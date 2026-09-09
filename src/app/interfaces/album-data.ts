9db17c6584ef602b3228680525334024b5070681
reboot0707 <bootcamp@contact.clam.tw>
2026-09-09 22:36:00 +0800

    feat: optimize album data interface

--- src/app/interfaces/album-data.ts
+++ src/app/interfaces/album-data.ts
@@ -1,6 +1,6 @@
 export interface AlbumData {
-  id: number,
-  albumName: string,
-  releaseDate: string,
-  albumType: string
+  id: number;
+  albumName: string;
+  releaseDate: string | null; // yyyy-MM-dd
+  albumType: string | null;
 }
