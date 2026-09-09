f0cd4b8d5bec37b1dee539781e3a42a4750c1be2
reboot0707 <bootcamp@contact.clam.tw>
2026-09-09 12:06:18 +0800

    feat(homepage): Hello Fruitbar8000!
    use router-outlet to place homepage components
    split header and footer as different component
    
    port mvc layout css to here.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./share/components/header/header";
+import { Footer } from "./share/components/footer/footer";
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
---
+++ src/app/components/homepage/homepage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-homepage',
+  styleUrl: './homepage.css',
+  templateUrl: './homepage.html',
+})
+export class Homepage {}
