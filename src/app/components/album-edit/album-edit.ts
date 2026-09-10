7cb71174ac125c7208c5c633f827a03e0d46742a
reboot0707 <bootcamp@contact.clam.tw>
2026-09-10 17:07:34 +0800

    feat(album,artist): 實作專輯、創作者編輯介面自動代入既有資料

--- src/app/components/album-edit/album-edit.ts
+++ src/app/components/album-edit/album-edit.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { AlbumService } from '../../services/album-service';
+import { AlbumData } from '../../interfaces/album-data';
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   selector: 'app-album-edit',
   styleUrl: './album-edit.css',
   templateUrl: './album-edit.html',
+  // Angular 22 預設使用 OnPush
+  changeDetection: ChangeDetectionStrategy.Eager,
 })
-export class AlbumEdit {}
+export class AlbumEdit implements OnInit {
+  formAlbumData: AlbumData = {
+    id: 0,
+    albumName: '',
+    albumType: '',
+    releaseDate: '',
+  }
+
+  formAlbumName = '';
+  formAlbumType: string | null = '';
+  formReleaseDate: string | null = '';
+
+  constructor(
+    private albumService: AlbumService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.loadAlbumDataById(this.activatedRoute.snapshot.params['id']) ?? {
+      id: 0,
+      albumName: '',
+      albumType: '',
+      releaseDate: '',
+    };
+  }
+
+  private loadAlbumDataById(id: number) {
+    this.albumService.getAlbumById(id).subscribe((data) => {
+      this.formAlbumData = data;
+      this.formAlbumName = data.albumName;
+      this.formAlbumType = data.albumType;
+      this.formReleaseDate = data.releaseDate;
+    });
+  }
+}
--- src/app/components/artist-edit/artist-edit.ts
+++ src/app/components/artist-edit/artist-edit.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, ActivatedRoute } from '@angular/router';
+import { ArtistData } from '../../interfaces/artist-data';
+import { ArtistService } from '../../services/artist-service';
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   selector: 'app-artist-edit',
   styleUrl: './artist-edit.css',
   templateUrl: './artist-edit.html',
+  // Angular 22 預設使用 OnPush
+  changeDetection: ChangeDetectionStrategy.Eager,
 })
-export class ArtistEdit {}
+export class ArtistEdit implements OnInit{
+
+  formArtistData: ArtistData = {
+    id: 0,
+    artistName: '',
+    artistType: ''
+  }
+
+  formArtistName = '';
+  formArtistType: string | null = '';
+
+  constructor(
+    private artistService: ArtistService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.loadArtistDataById(this.activatedRoute.snapshot.params['id']) ?? {
+      id: 0,
+      artistName: '',
+      artistType: ''
+    };
+  }
+
+  private loadArtistDataById(id: number) {
+    this.artistService.getArtistById(id).subscribe((data) => {
+      this.formArtistData = data;
+      this.formArtistName = data.artistName;
+      this.formArtistType = data.artistType;
+    });
+  }
+}
